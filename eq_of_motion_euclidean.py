260ed0f16883e90aa67134e7d8b6b0148e27ea56
indoor-industry <emiliano.mario@outlook.com>
2023-04-27 01:26:12 +0200

    new ex

--- eq_of_motion_euclidean.py
+++ eq_of_motion_euclidean.py
@@ -7,9 +7,9 @@ from numba import njit
 
 N = 100
 w_0 = 1
-a = np.linspace(0.05, 0.8, 100)
+a = np.linspace(0.1, 0.5, 10)
 xi = 1
-xf = 1
+xf = 0
 m = 1
 
 #define matrix of coefficients of unimproved action eq of motion
@@ -23,7 +23,7 @@ def coeff_matrix(a):
         jp = (j+1)
         if jm >= 0:
             A[jm][j] = 1
-        A[j][j] = -(2 - a**2*w_0**2)
+        A[j][j] = -(2 + a**2*w_0**2)
         if jp < N:
             A[jp][j] = 1
     return A
@@ -43,7 +43,7 @@ def coeff_matrix_imp(a):
             A[jm2][j] = -1/12
         if jm >= 0:
             A[jm][j] = 4/3
-        A[j][j] = -(5/2 - a**2*w_0**2)
+        A[j][j] = -(5/2 + a**2*w_0**2)
         if jp < N:
             A[jp][j] = 4/3
         if jp2 < N:
@@ -69,8 +69,8 @@ def coeff_matrix_imp_transformed(a):
             A[jp2][j] = -1/12
     return A
 
-def sincos(t, S, w, C):
-    return S*np.sin(w*t)+C*np.cos(w*t)
+def exp(t, A, w, phi):
+    return A*np.exp(-w*t+phi)
     
 @njit
 def values(a):
@@ -85,15 +85,7 @@ def values(a):
         return B
 
 ws2 = np.empty(len(a))
-Ss = np.empty(len(a))
-Cs = np.empty(len(a))
-
 ws2_imp = np.empty(len(a))
-Ss_imp = np.empty(len(a))
-Cs_imp = np.empty(len(a))
-
-Es = np.empty(len(a))
-Es_imp = np.empty(len(a))
 for i in range(len(a)):
 
     A = coeff_matrix(a[i])
@@ -106,41 +98,20 @@ for i in range(len(a)):
     t = np.arange(0, N, 1)*a[i]
     plt.scatter(t, X)
 
-    param, cov = optimize.curve_fit(sincos, t, X, p0=(1, 1, 1))
-    param_imp, cov_imp = optimize.curve_fit(sincos, t, X_imp, p0=(1, 1, 1))
+    param, cov = optimize.curve_fit(exp, t, X, p0=(0.1, 1, 0))
+    param_imp, cov_imp = optimize.curve_fit(exp, t, X_imp, p0=(0.1, 1, 0))
 
-    fit = [sincos(x, param[0], param[1], param[2]) for x in t]
+    fit = [exp(x, param[0], param[1], param[2]) for x in t]
     plt.plot(t, fit, label=f'a={a[i]}')
 
-    #energy of HO
-    E = 0.5*m*w_0**2*(param[0]**2+param[2]**2)
-    E_imp = 0.5*m*w_0**2*(param_imp[0]**2+param_imp[2]**2)
-
-    Es[i] = E
-    Ss[i] = param[0]
     ws2[i] = param[1]**2
-    Cs[i] = param[2]
-
-    Es_imp[i] = E_imp
-    Ss_imp[i] = param_imp[0]
     ws2_imp[i] = param_imp[1]**2
-    Cs_imp[i] = param_imp[2]
 
     #print(i)
-plt.legend()
-plt.show()
-
-plt.plot(a, Ss, label='S sin coefficient')
-plt.plot(a, Cs, label='C cos coefficient')
-plt.plot(a, Ss_imp, label='S sin coefficient improved')
-plt.plot(a, Cs_imp, label='C cos coefficient improved')
-plt.plot(a, Es, label='energy')
-plt.plot(a, Es_imp, label='energy improved')
-plt.legend()
 plt.show()
 
 #expected behaviour for unimproved action
-exp = [w_0**2*(1+((q*w_0)**2)/12) for q in a]
+exp = [w_0**2*(1-((q*w_0)**2)/12) for q in a]
 exp_imp = [w_0**2*(1+((q*w_0)**4)/90) for q in a]
 
 
